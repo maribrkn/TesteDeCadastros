fc3743ff047ce6a631e862b20c5d88542ddbcc5f
maribrkn <yasurimin@gmail.com>
2025-05-11 14:31:19 -0300

    first commit

---
+++ src/main/java/dev/java10x/TesteDeCadastros/TesteDeCadastrosApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.TesteDeCadastros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDeCadastrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDeCadastrosApplication.class, args);
+	}
+
+}
