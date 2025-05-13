84a677e7d9e27b582e7d9eba3d9ddfa56ae05c51
maribrkn <yasurimin@gmail.com>
2025-05-13 16:10:47 -0300

    feat: Coloca o Lombok na classe para abstração

--- src/main/java/dev/java10x/TesteDeCadastros/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/java10x/TesteDeCadastros/Ninjas/Model/NinjaModel.java
@@ -1,13 +1,20 @@
 package dev.java10x.TesteDeCadastros.Ninjas.Model;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor // Cria construtor vazio
+@AllArgsConstructor // Criar construtores com argumentos
+@Data // Criar getters e setters
+
 public class NinjaModel {
-//  JPA Java Persistency API;
+    //  JPA Java Persistency API;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
@@ -18,59 +25,5 @@ public class NinjaModel {
     @ManyToOne // Um ninja só pode ter uma missão
     @JoinColumn(name = "missoes_id") // Foreign key - chave estrangeiragit
     private MissoesModel missoes;
-
-    public MissoesModel getMissoes() {
-        return missoes;
-    }
-
-    public void setMissoes(MissoesModel missoes) {
-        this.missoes = missoes;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public NinjaModel() {
-    }
-    public NinjaModel(int idade, String nome, String email) {
-        this.idade = idade;
-        this.nome = nome;
-        this.email = email;
-    }
-    public NinjaModel(Long id, int idade, String nome, String email, MissoesModel missoes) {
-        this.id = id;
-        this.idade = idade;
-        this.nome = nome;
-        this.email = email;
-        this.missoes = missoes;
-    }
 }
+
